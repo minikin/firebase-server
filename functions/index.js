0a920af52dc21356f0d7d1ad8f6a0a9f0ae4009d
minikin <djminikin@gmail.com>
2020-04-30 13:17:06 +0200

    Add custom index.html

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,12 @@
 const functions = require('firebase-functions');
-const jsonServer = require('json-server')
-const data = require('../public/db.json')
+const jsonServer = require('json-server');
+const data = require('../public/db.json');
 
-const port = process.env.PORT || 4000;
-const router = jsonServer.router(data)
-const server = jsonServer.create()
+const router = jsonServer.router(data);
+const server = jsonServer.create();
 
-server.use(jsonServer.defaults(['../public']))
-server.use(router)
-server.listen(port)
+server.use(jsonServer.defaults(['./public']));
+server.use(router);
 
 exports.api = functions.https.onRequest(server);
 
